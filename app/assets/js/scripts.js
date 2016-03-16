65d6ba04fe8e5a31f661afb974bf697ca2b5e61c
Hamish Irving <hamishirving@Hamishs-MacBook-Pro.local>
2016-03-16 07:36:28 +0000

    First commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,73 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://HosseinKarami.github.io/fastshell
+ * @author Hossein Karami
+ * @version 1.0.5
+ * Copyright 2016. MIT licensed.
+ */
+// DOM Ready
+$( document ).ready(function() {
+	'use strict';
+
+    // ----
+    // Show Current Page in Nav
+    // ----
+
+    $(function(){
+      $('.nav ul li').each(function() {
+        if ($(this).prop('href') == window.location.href) {
+          $(this).addClass('current');
+        }
+      });
+    });
+
+	// ----
+	// Mobile Navigation - Slideout.js
+	// ----
+
+    // Toggle button
+	document.querySelector('.toggle-button').addEventListener('click', function() {
+	slideout.toggle();
+	});
+	// Fixed Nav
+	slideout.on('beforeopen', function() {
+	document.querySelector('.nav').classList.add('fixed-open');
+		// Fix issue on iOS where navbar translated to 0 first
+		$('.nav').velocity({
+			translateX: '-256'
+		}, 0.1);
+	});
+	slideout.on('beforeclose', function() {
+	document.querySelector('.nav').classList.remove('fixed-open');
+		// Fix issue on iOS where navbar translated to 0 first
+		$('.nav').velocity({
+			translateX: '0'
+		}, 0.1);
+	});
+
+	// Hamburger animation
+	$('#hamburger').click(function() {
+		$('.top-menu').toggleClass('top-animate');
+		$('.mid-menu').toggleClass('mid-animate');
+		$('.bottom-menu').toggleClass('bottom-animate');
+	});
+
+	// ----
+    // Swiper
+    // ----
+    var mySwiper = new Swiper ('.swiper-container', {
+		// Optional parameters
+		autoplay: 3000,
+		effect: 'fade',
+		loop: true
+	});        
+
+// End DOM Ready
+});
+
+// All content loaded (inc images, css etc)
+window.onload = function() {
+
+// End Window Ready
+};
